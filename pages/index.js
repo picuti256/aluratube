15de0989e66f63553419dab838b2d51e53c1e6c1
Giovanne Piçuti <picuti256@gmail.com>
2022-11-10 09:25:14 -0300

    Primeiro commit relacionado a imersão alura

---
+++ pages/index.js
@@ -0,0 +1,86 @@
+import config from "../config.json";
+import styled from "styled-components"
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+
+function HomePage() {
+  const estiloDaHomePage = {  }
+
+  return (
+    <>
+    <CSSReset />
+    <div style={estiloDaHomePage}>
+      <Menu />
+      <Header />
+      <TimeLine playlists={config.playlists} />
+    </div>
+    </>
+  );
+}
+
+export default HomePage
+
+
+const StyledHeader = styled.div`
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+  }
+  .user-info {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    padding: 16px 32px;
+    gap: 16px;
+    margin-top: 50px;
+  }
+`;
+function Header() {
+  return (
+    <StyledHeader>
+      {/* <img src="banner" /> */}
+      <section className="user-info">
+        <img src={`https://github.com/${config.github}.png`} />
+        <div>
+          <h2>
+            {config.name}
+          </h2>
+          <p>
+            {config.job}
+          </p>
+        </div>
+      </section>
+    </StyledHeader>
+  )
+}
+
+
+function TimeLine(props) {
+  const playlistNames = Object.keys(props.playlists);
+  return (
+    <StyledTimeline>
+      {playlistNames.map((playlistNames) => {
+        const videos = props.playlists[playlistNames];
+        return (
+          <section>
+            <h2>{playlistNames}</h2>
+            <div>
+              {videos.map((video) => {
+                return (
+                  <a href={video.url}>
+                    <img src={video.thumb} />
+                    <span>
+                      {video.title}
+                    </span>
+                  </a>
+                )
+              })};
+            </div>
+          </section>
+        )
+      })}
+    </StyledTimeline>
+  )
+}
